5ace0d028c34409afb18bc5bf7bc611b30ede313
Paweł Karaś <pkaras.it@gmail.com>
2021-02-18 16:59:37 +0100

    Deploy test

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,13 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img src="./assets/deploy.png" width="250">
+    <h1>A Github Pages VueJS deploy test</h1>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'App'
 }
 </script>
 
